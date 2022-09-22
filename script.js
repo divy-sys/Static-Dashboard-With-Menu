ff100b92a0c4505d27817de7c01d0379b4461278
divy-sys <divy.varia.1020@gmail.com>
2022-09-22 17:50:56 +0530

    Static Dashboard Init commit
    Static Dashboard Init commit.

---
+++ script.js
@@ -0,0 +1,43 @@
+
+
+
+function formdivy() {
+    let a = document.getElementById("formsofform").style.display;
+    if (a === "none") {
+        document.getElementById("formsofform").style.display = "block";
+    }
+    else if (a === "block") {
+        document.getElementById("formsofform").style.display = "none";
+    }
+
+}
+function tables() {
+    let a = document.getElementById("tablesoftable").style.display;
+    if (a === "none") {
+        document.getElementById("tablesoftable").style.display = "block";
+    }
+    else if (a === "block") {
+        document.getElementById("tablesoftable").style.display = "none";
+    }
+
+}
+function charts() {
+    let a = document.getElementById("chartsofchart").style.display;
+    if (a === "none") {
+        document.getElementById("chartsofchart").style.display = "block";
+    }
+    else if (a === "block") {
+        document.getElementById("chartsofchart").style.display = "none";
+    }
+
+}
+function js() {
+    let a = document.getElementById("jsofjs").style.display;
+    if (a === "none") {
+        document.getElementById("jsofjs").style.display = "block";
+    }
+    else if (a === "block") {
+        document.getElementById("jsofjs").style.display = "none";
+    }
+
+}
